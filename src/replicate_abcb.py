eb77fc04de97410af8e5cf31ed8c86222ac2f879
okolukisa <oguzkolukisa1@gmail.com>
2026-01-03 21:18:02 +0300

    .

--- src/replicate_abcb.py
+++ src/replicate_abcb.py
@@ -145,7 +145,7 @@ def main() -> None:
             "train_episodes": 20000,
             "val_episodes": 2000,
             "epochs": 250,
-            "batch_size": 256,
+            "batch_size": 128,
             "base_lr": 0.002,
             "crop_size": 473,
             "num_workers": 4,
